530886f33a9a3450839974f8d129d6f056822e96
Alan Correa <am.correagarcia@gmail.com>
2026-05-12 18:54:22 -0600

    feat: add Footer with contact links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,22 @@
 function Footer() {
-  return <div>Footer</div>
+  return (
+    <footer id="contact">
+      <p>Let's connect</p>
+      <ul>
+        <li>
+          <a href="https://github.com/twiinko-rocker" target="_blank" rel="noreferrer">
+            GitHub
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/alan-correa-8562b1b0/" target="_blank" rel="noreferrer">
+            LinkedIn
+          </a>
+        </li>
+      </ul>
+      <p>© 2026 Alan C. Built with React.</p>
+    </footer>
+  )
 }
 
-export default Footer   
+export default Footer
