e4cfd8b6ec28a10fd899f3f598e1c6d7e4c687f3
Alan Correa <am.correagarcia@gmail.com>
2026-05-12 18:24:05 -0600

    feat: add Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,14 @@
 function Navbar() {
-  return <div>Navbar</div>
+  return (
+    <nav>
+        <span>Alan</span>
+        <ul>
+            <li><a href="#about">About</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+    </nav>
+  )
 }
 
 export default Navbar
