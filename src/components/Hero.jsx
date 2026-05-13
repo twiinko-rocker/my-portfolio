7ff01935bad1966a5b9cf0fad489be5b4076df08
Alan Correa <am.correagarcia@gmail.com>
2026-05-12 18:27:53 -0600

    feat: aDD HERO COMPONENT

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,11 @@
 function Hero() {
-  return <div>Hero</div>
+  return (
+    <section id="hero">
+      <h1>Hi, I'm Alan</h1>
+      <p>Junior Developer. Problem solver. Builder.</p>
+      <a href="#projects">See My Work</a>
+    </section>
+  )
 }
 
 export default Hero        
